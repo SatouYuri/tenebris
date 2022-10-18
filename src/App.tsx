1ee5f1045a6ab87900d072c597c0492c2c116444
Yuri Sato <yuri.sato@stoom.com.br>
2022-10-17 21:50:06 -0300

    feat: Primeiro commit

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Native é um lixokkkkkkkkkkkkkkkkkkkk
         </p>
         <a
           className="App-link"
