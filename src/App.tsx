2f20f702df86558685493e7552c94401209e432c
SatouYuri <yurimetal4d@gmail.com>
2022-10-24 23:05:36 -0300

    feat: Adcionando botões +/- no componente CountBar. Removendo zoom do navegador na aplicação em mobile. Algumas estilizações em CountBar foram implementadas.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import Sheet from './components/Sheet'
 import './App.css';
+import {Helmet} from "react-helmet";
 
 function App() {
   return (
     <>
+      <Helmet>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"></meta>
+      </Helmet>
       <Sheet />
     </>
   );
--- src/components/CountBar/index.tsx
+++ src/components/CountBar/index.tsx
@@ -2,8 +2,14 @@ import "./styles.css"
 
 export default function CountBar() {
   return (
-    <div className="counterBackground">
-      <div className="counterFront"></div>
+    <div className="countBar">
+      <button className="countButton">-</button>
+      <div className="countBackground">
+        <div className="countFront">
+          <div className="countFrontColor" />
+        </div>
+      </div>
+      <button className="countButton">+</button>
     </div>
   );
 }
