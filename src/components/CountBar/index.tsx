6d017b6f0bbdcb51bd74856b98e8cca491ac7cdb
SatouYuri <yurimetal4d@gmail.com>
2022-11-01 21:26:56 -0300

    Ossos ao mar...

--- src/components/CountBar/index.tsx
+++ src/components/CountBar/index.tsx
@@ -2,24 +2,38 @@ import React, { useEffect, useState } from "react";
 import "./styles.css"
 
 type CountBarProps = {
+  id: string;
   countVal: number;
   maxCountVal: number;
   color?: string;
+  settingMax?: boolean;
 }; 
 
-const CountBar = ({ countVal, maxCountVal, color }: CountBarProps) => {
-  const [count, setCount] = useState<number>(countVal);
+const CountBar = ({ id, countVal, maxCountVal, color, settingMax }: CountBarProps) => {
+  const [countBarId] = useState(`CountBar@${id}`);
+  const [count, setCount] = useState<number>(Number(localStorage.getItem(`${countBarId}_count`)) || countVal);
+  const [maxCount, setMaxCount] = useState<number>(Number(localStorage.getItem(`${countBarId}_maxCount`)) || maxCountVal); //Remover mais tarde...
 
   const increment = (amount: number) => {
     const newCountVal = count + amount;
     if(amount > 0 || (amount < 0 && newCountVal >= 0)){
       setCount(newCountVal);
+      localStorage.setItem(`${countBarId}_count`, JSON.stringify(newCountVal));
+    }
+  };
+
+  //Remover mais tarde...
+  const incrementMax = (amount: number) => {
+    const newCountVal = maxCount + amount;
+    if(amount > 0 || (amount < 0 && newCountVal >= 0)){
+      setMaxCount(newCountVal);
+      localStorage.setItem(`${countBarId}_maxCount`, JSON.stringify(newCountVal));
     }
   };
 
   const getBarProgressStyle = () => {
     const barProgressStyle: React.CSSProperties = {
-      'width': (100 * count/maxCountVal).toString() + '%',
+      'width': (100 * count/maxCount).toString() + '%',
     }
     return barProgressStyle;
   }
@@ -32,7 +46,7 @@ const CountBar = ({ countVal, maxCountVal, color }: CountBarProps) => {
       return countBarStyle;
     }else if(color === "blue"){
       const countBarStyle: React.CSSProperties = {
-        'backgroundColor': 'rgba(0, 0, 140, 0.8)',
+        'backgroundColor': 'rgba(0, 0, 240, 0.8)',
       }
       return countBarStyle;
     }else if(color === "yellow"){
@@ -40,19 +54,24 @@ const CountBar = ({ countVal, maxCountVal, color }: CountBarProps) => {
         'backgroundColor': 'rgba(140, 140, 0, 0.8)',
       }
       return countBarStyle;
+    }else if(color === "edit"){
+      const countBarStyle: React.CSSProperties = {
+        'backgroundColor': 'rgba(0, 240, 240, 0.8)',
+      }
+      return countBarStyle;
     }
   };
 
   return (
     <div className="countBar">
-      <button className="countButton" onClick={() => increment(-1)}>-</button>
+      <button className="countButton" onClick={() => {settingMax ? incrementMax(-1) : increment(-1)}}>-</button>
       <div className="countBackground">
         <div className="countFront" style={getBarProgressStyle()}>
           <div className="countFrontColor" style={getBarColorStyle()} />
         </div>
       </div>
-      <p className="counter">{count}/{maxCountVal}</p>
-      <button className="countButton" onClick={() => increment(1)}>+</button>
+      <p className="counter">{count}/{maxCount}</p>
+      <button className="countButton" onClick={() => {settingMax ? incrementMax(1) : increment(1)}}>+</button>
     </div>
   );
 }
--- src/components/Sheet/index.tsx
+++ src/components/Sheet/index.tsx
@@ -2,16 +2,21 @@ import "./styles.css"
 import Attributes from "../Attributes";
 import CountBar from "../CountBar";
 import AtkList from "../AtkList";
+import React, { useEffect, useState } from "react";
 
 export default function Sheet() {
+  const [settingMax, setSettingMax] = useState<boolean>(Boolean(localStorage.getItem("Sheet@Main_settingMax")) || false);
+
   return (
     <div className="sheetContainer">
-      <h1 className="charName">ISAAC NICARELLI DOS SANTOS SILVA</h1>
-      <Attributes />
-      <CountBar countVal={24} maxCountVal={32} color="red"/>
-      <CountBar countVal={20} maxCountVal={32} color="blue"/>
-      <CountBar countVal={16} maxCountVal={32} color="yellow"/>
-      <AtkList />
+      <h1 className="charName">Ossos ao Mar</h1>
+      <img className="bonesPromoImage" src="https://images.fineartamerica.com/images-medium-large-5/haunted-beach-mihai-florea.jpg"></img>
+      {/* <Attributes /> */}
+      <CountBar id="Health" countVal={24} maxCountVal={32} color={settingMax ? "edit" : "red"} settingMax={settingMax} />
+      <CountBar id="Sanity" countVal={20} maxCountVal={32} color={settingMax ? "edit" : "blue"} settingMax={settingMax} />
+      <CountBar id="Stamina" countVal={16} maxCountVal={32} color={settingMax ? "edit" : "yellow"} settingMax={settingMax} />
+      <button className="editMax" onClick={() => setSettingMax(!settingMax)}>Alterar Máximos</button> 
+      {/* <AtkList /> */}
     </div>
   );
 }
